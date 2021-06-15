64c5a27a386e006f1b079a67b2874dca22651720
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-15 21:09:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-class Tree(){
+class Tree{
   constructor(species){
     this.species = species;
   }
   
   static definition(){
-    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
   }
+}
+
+class Deciduous extends Tree{
+  super(species)
+  constructor(name){
+    this.name = name;
+  }
+  
 }
